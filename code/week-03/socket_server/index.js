6a7c1babb0950ac651ef87f4b3cd3f3d984f24df
mmminason <mina1822@gmail.com>
2024-02-08 21:04:31 -0500

    week4 update

---
+++ code/week-03/socket_server/index.js
@@ -0,0 +1,18 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({port: 3000});
+
+wss.on('connection', onConnection);
+
+
+function onConnection(ws){
+    ws.on('message', onMessage);
+}
+
+function onMessage(data){
+    //let jsonData = JSON.parse(data);
+    let jd = JSON.parse(data);
+    wss.clients.forEach(client => {
+        client.send(JSON.stringify(jd));       
+    });
+}
