8bda8abf6bb478a9d535e554651678a774dea3b7
mmminason <mina1822@gmail.com>
2024-02-28 02:13:43 -0500

    240228 update

--- Practice/00-Note/note.js
+++ Practice/00-Note/note.js
@@ -2,9 +2,20 @@
 
 API
 
-
-
 Web APIs
 HTTP(Hypertext Transfer Protocol)
 
 
+-JSON-
+> simplest data interchange format
+> lightweight text-based structure
+> easy to read
+> key-value pairs
+> used for serializaton and transmission of data between the netwrok the network connection
+> independent programming language and platform
+
+
+
+
+
+*/
